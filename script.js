dcd7d7984da1ad8a315fced2f4e0885e5d827752
mi00 <144072647+mi-0698@users.noreply.github.com>
2025-06-07 13:39:19 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('in-view');
+      }
+    });
+  }, {
+    threshold: 0.1
+  });
+
+  document.querySelectorAll('.image-placeholder, .text').forEach(el => {
+    observer.observe(el);
+  });
+});
